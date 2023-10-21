ed9b50f53b2172a6889f6c9117124926b55a8c56
Be1uga4life <achtungbabyboiii@gmail.com>
2023-10-21 15:33:09 -0700

    cors and bad gateway pains me

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 app.debug = True
 
 # Allow CORS from any origin
-CORS(app, resources={r"/messageDB": {"origins": "https://whispbackend.duckdns.org/"}}
-CORS(app, resources={r"/login": {"origins": "https://whispbackend.duckdns.org/"}}
+CORS(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
@@ -206,7 +205,3 @@ def login():
     except Exception as e:
         return jsonify({'success': False, 'message': 'An error occurred during login'})
 
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888)
